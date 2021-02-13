816d38f23277afd472b0111e091ced604644b314
Inês Martins <in.afmartins@gmail.com>
2021-02-13 13:20:13 +0000

    dry that code

--- bf_vhosts.py
+++ bf_vhosts.py
@@ -7,6 +7,11 @@ def is_valid_file(parser, arg):
     else:
         return arg
 
+def found_vhost(vhost):
+	print("Found vhost: {vhost}")
+	if args.out_file is not None:
+		os.system("echo {vhost} >> {args.out_file}")
+
 # parses arguments
 parser = argparse.ArgumentParser(description='Brute-forces vhosts for given --ip and based on specified domain and wordlist')
 parser.add_argument('-i', '--ip-address', dest='ip', type=str, required=True)
@@ -24,17 +29,9 @@ f = open(args.wordlist, "r")
 words = f.read().split("\n")
 
 for word in words:
-	req = "curl -H \'Host: " + word + "." + args.domain + "\' " + args.ip
+	req = "curl -H \'Host: {word}.{args.domain}\'{args.ip}"
 	res = os.popen(req).read()
 
 	# user specified success page content
 	if args.success is not None and args.success in res:
-		print("Found vhost " + word + "." + args.domain)
-		if args.out_file is not None:
-			os.system("echo " + word + "." + args.domain >> args.out_file)
-
-	# user specified error page content
-	if args.failure is not None and args.failure not in res:
-		print("Found vhost " + word + "." + args.domain)
-		if args.out_file is not None:
-			os.system("echo " + word + "." + args.domain >> args.out_file)
+		found_vhost("{word}.{args.domain}")
