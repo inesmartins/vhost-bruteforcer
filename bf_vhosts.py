c87a5146d73a40fe470274e884d1ce3d44c640d3
Inês Martins <in.afmartins@gmail.com>
2021-02-13 13:40:17 +0000

    supress curl output

--- bf_vhosts.py
+++ bf_vhosts.py
@@ -7,10 +7,15 @@ def is_valid_file(parser, arg):
     else:
         return arg
 
+def nullable_string(val):
+    if not val:
+        return None
+    return val
+
 def found_vhost(vhost, out_file):
-	print('Found vhost: {vhost}')
+	print('Found vhost: ' + vhost)
 	if args.out_file is not None:
-		os.system('echo {vhost} >> {out_file}')
+		os.system('echo ' + vhost + ' >> ' + out_file)
 
 # parses arguments
 parser = argparse.ArgumentParser(description='Brute-forces vhosts for given --ip and based on specified domain and wordlist')
@@ -30,32 +35,35 @@ parser.add_argument('-w', '--wordlist',
 parser.add_argument('-s', '--success', 
 					dest='success', 
 					help='specifies the content that will be compared agains the server response to find vhosts', 
-					type=str, required=False)
+					type=nullable_string, required=False)
 parser.add_argument('-f', '--failure', 
 					dest='failure', 
 					help='specifies the content that will be compared agains the server response to discard vhosts', 
-					type=str, required=False)
+					type=nullable_string, required=False)
 parser.add_argument('-o', '--output-file', 
 					dest='out_file', 
 					help='the script can write all vhosts to this file', 
 					metavar='FILE', 
-					type=str, required=True)
+					type=nullable_string, required=False)
 args = parser.parse_args()
 
 if args.success is None and args.failure is None:
 	parser.error('Please specify either success (-s/--success) or failure (-f/--failure) content')
 
+print("Brute-force in progress ...")
+
 f = open(args.wordlist, "r")
 words = f.read().split("\n")
 
 for word in words:
-	req = "curl -H \'Host: {word}.{args.domain}\'{args.ip}"
+	vhost = word + '.' + args.domain
+	req = "curl -H \'Host: " + vhost + "\' " + args.ip + " --silent"
 	res = os.popen(req).read()
 
 	# user specified success page content
 	if args.success is not None and args.success in res:
-		found_vhost("{word}.{args.domain}", args.out_file)
+		found_vhost(vhost, args.out_file)
 
 	# user specified success page content
-	if args.failure is not None and args.success not in res:
-		found_vhost("{word}.{args.domain}", args.out_file)
+	if args.failure is not None and args.failure not in res:
+		found_vhost(vhost, args.out_file)
