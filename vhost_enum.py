eb883aeb2e1aa36010d3ca9ee5d40f988c43f44b
Inês Martins <in.afmartins@gmail.com>
2021-02-13 13:14:33 +0000

    finishes tweaks

--- vhost_enum.py
+++ vhost_enum.py
@@ -5,7 +5,7 @@ def is_valid_file(parser, arg):
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
-        return open(arg, 'r')  # return an open file handle
+        return arg
 
 # parses arguments
 parser = argparse.ArgumentParser(description='Brute-forces vhosts for given --ip and based on specified domain and wordlist')
@@ -14,21 +14,27 @@ parser.add_argument('-d', '--domain', dest='domain', type=str, required=True)
 parser.add_argument('-w', '--wordlist', dest='wordlist', metavar="FILE", type=lambda x: is_valid_file(parser, x), required=True)
 parser.add_argument('-s', '--success', dest='success', type=str, required=False)
 parser.add_argument('-f', '--failure', dest='failure', type=str, required=False)
-parser.add_argument('-o', '--output-file', dest='out_file', metavar='FILE', type=lambda x: is_valid_file(parser, x), required=True)
+parser.add_argument('-o', '--output-file', dest='out_file', metavar='FILE', type=str, required=True)
 args = parser.parse_args()
+
+if args.success is None and args.failure is None:
+	parser.error('Please specify either success (-s/--success) or failure (-f/--failure) content')
 
 f = open(args.wordlist, "r")
 words = f.read().split("\n")
+
 for word in words:
 	req = "curl -H \'Host: " + word + "." + args.domain + "\' " + args.ip
 	res = os.popen(req).read()
-	if args.success != NULL:
-		if args.success in res:
-			print("Found vhost " + word + "." + args.domain)
-			if args.out_file != NULL:
-				os.system("echo " + word + "." + args.domain  >> out_file)
-	if args.failure != NULL:
-		if args.failure not in res:
-			print("Found vhost " + word + "." + args.domain)
-                        if args.out_file != NULL:
-                                os.system("echo " + word + "." + args.domain$
+
+	# user specified success page content
+	if args.success is not None and args.success in res:
+		print("Found vhost " + word + "." + args.domain)
+		if args.out_file is not None:
+			os.system("echo " + word + "." + args.domain >> args.out_file)
+
+	# user specified error page content
+	if args.failure is not None and args.failure not in res:
+		print("Found vhost " + word + "." + args.domain)
+		if args.out_file is not None:
+			os.system("echo " + word + "." + args.domain >> args.out_file)
